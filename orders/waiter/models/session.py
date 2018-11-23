16998f035a1f2843f4c27dbb575431cb762f878f
bumbleblo <bumbleblo2013@gmail.com>
2018-11-23 05:13:26 -0200

    #3 - Adding order model

--- orders/waiter/models/session.py
+++ orders/waiter/models/session.py
@@ -4,3 +4,17 @@ from .orderpad import OrderPad
 
 class Session(models.Model):
     orderpad = models.ForeignKey(OrderPad, on_delete=models.DO_NOTHING)
+
+class Order(models.Model):
+    """
+    Model for Order representation in this microservice
+    """
+
+    session = models.ForeignKey(Session, on_delete=models.DO_NOTHING)
+
+    product = models.IntegerField()
+    state = models.CharField(max_lenght=12)
+    table = models.IntegerField()
+    time = models.TimeField()
+    date = models.DateField()
+    order_type = models.CharField(max_lenght=12)
